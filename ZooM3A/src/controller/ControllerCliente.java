a23cd01a0299dc788068e55ea645199a3aa0dbba
Alejo2412qwe <coraspe.alejandro2@gmail.com>
2022-09-12 19:09:47 -0500

    controllercliente

---
+++ ZooM3A/src/controller/ControllerCliente.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.viewRegistrarCliente;
+import view.viewVistaCliente;
+
+/**
+ *
+ * @author ALEJO
+ */
+public class ControllerCliente {
+
+    private viewRegistrarCliente vrc = new viewRegistrarCliente();
+    private viewVistaCliente vvc = new viewVistaCliente();
+
+    
+    
+}
