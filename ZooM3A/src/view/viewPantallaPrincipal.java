731106c72c56bb0b099edbd572b065e5234a6d23
D4N1EL <dannyandrade989@gmail.com>
2022-09-20 08:11:13 -0500

    Clase cliente

--- ZooM3A/src/view/viewPantallaPrincipal.java
+++ ZooM3A/src/view/viewPantallaPrincipal.java
@@ -38,15 +38,21 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
 
         jPanel3 = new javax.swing.JPanel();
         jDPprincipal = new javax.swing.JDesktopPane();
-        jPanel1 = new javax.swing.JPanel();
-        btnCerrarSesion = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jlblrolPP = new javax.swing.JLabel();
-        jlblNombrePP = new javax.swing.JLabel();
         jlblCedulaPP = new javax.swing.JLabel();
+        jlblNombrePP = new javax.swing.JLabel();
         jlblFotoPP = new javax.swing.JLabel();
         jlblCedulaPP1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        btnCerrarSesion = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         barramenus = new javax.swing.JMenuBar();
         agregarpersona = new javax.swing.JMenu();
@@ -93,20 +99,6 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
 
         jPanel3.add(jDPprincipal, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 0, 970, 630));
 
-        jPanel1.setBackground(new java.awt.Color(144, 183, 125));
-
-        btnCerrarSesion.setBackground(new java.awt.Color(117, 156, 98));
-        btnCerrarSesion.setFont(new java.awt.Font("MS Gothic", 0, 24)); // NOI18N
-        btnCerrarSesion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cerrar-sesion (1).png"))); // NOI18N
-        btnCerrarSesion.setText("CERRAR SESION");
-        btnCerrarSesion.setToolTipText("Vaciar pantalla");
-        btnCerrarSesion.setBorder(null);
-        btnCerrarSesion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnCerrarSesionActionPerformed(evt);
-            }
-        });
-
         jPanel2.setBackground(new java.awt.Color(72, 56, 56));
         jPanel2.setForeground(new java.awt.Color(210, 215, 159));
 
@@ -114,17 +106,17 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(210, 215, 159));
         jLabel3.setText("¡ZOOMANIA!");
 
-        jlblrolPP.setFont(new java.awt.Font("MS Gothic", 1, 22)); // NOI18N
+        jlblrolPP.setFont(new java.awt.Font("MS Gothic", 1, 21)); // NOI18N
         jlblrolPP.setForeground(new java.awt.Color(210, 215, 159));
         jlblrolPP.setText("SECRETARIA");
-
-        jlblNombrePP.setFont(new java.awt.Font("MS Gothic", 1, 12)); // NOI18N
-        jlblNombrePP.setForeground(new java.awt.Color(210, 215, 159));
-        jlblNombrePP.setText("Nombre usuario");
 
         jlblCedulaPP.setFont(new java.awt.Font("MS Gothic", 1, 12)); // NOI18N
         jlblCedulaPP.setForeground(new java.awt.Color(210, 215, 159));
         jlblCedulaPP.setText("Cedula");
+
+        jlblNombrePP.setFont(new java.awt.Font("MS Gothic", 1, 12)); // NOI18N
+        jlblNombrePP.setForeground(new java.awt.Color(210, 215, 159));
+        jlblNombrePP.setText("Nombre usuario");
 
         jlblFotoPP.setBackground(new java.awt.Color(72, 56, 56));
         jlblFotoPP.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
@@ -139,30 +131,28 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jlblFotoPP, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(9, 9, 9)
-                        .addComponent(jlblrolPP, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGap(15, 15, 15)
+                        .addComponent(jlblFotoPP, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jlblNombrePP, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(jlblCedulaPP1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jlblCedulaPP, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addContainerGap(11, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(52, 52, 52))
+                            .addComponent(jlblrolPP, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jlblNombrePP, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(jPanel2Layout.createSequentialGroup()
+                                    .addComponent(jlblCedulaPP1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(jlblCedulaPP, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(56, 56, 56)
+                        .addComponent(jLabel3)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(22, Short.MAX_VALUE)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -175,33 +165,98 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
                             .addComponent(jlblCedulaPP, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jlblCedulaPP1, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addComponent(jlblFotoPP, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(35, 35, 35))
+                .addGap(19, 19, 19))
         );
 
-        jButton1.setText("jButton1");
+        jPanel3.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 240, -1));
+
+        jPanel1.setBackground(new java.awt.Color(144, 183, 125));
+
+        btnCerrarSesion.setBackground(new java.awt.Color(117, 156, 98));
+        btnCerrarSesion.setFont(new java.awt.Font("MS Gothic", 0, 24)); // NOI18N
+        btnCerrarSesion.setForeground(new java.awt.Color(204, 255, 204));
+        btnCerrarSesion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cerrar-sesion (1).png"))); // NOI18N
+        btnCerrarSesion.setText("CERRAR SESION");
+        btnCerrarSesion.setToolTipText("Vaciar pantalla");
+        btnCerrarSesion.setBorder(null);
+
+        jButton2.setBackground(new java.awt.Color(144, 183, 125));
+        jButton2.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/datos-del-usuario (1).png"))); // NOI18N
+        jButton2.setText("Personal");
+        jButton2.setBorder(null);
+
+        jButton3.setBackground(new java.awt.Color(144, 183, 125));
+        jButton3.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/alimentos-para-mascotas (3).png"))); // NOI18N
+        jButton3.setText("Alimento");
+        jButton3.setBorder(null);
+
+        jButton4.setBackground(new java.awt.Color(144, 183, 125));
+        jButton4.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/dieta (1).png"))); // NOI18N
+        jButton4.setText("Dieta");
+        jButton4.setBorder(null);
+
+        jButton5.setBackground(new java.awt.Color(144, 183, 125));
+        jButton5.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/repartidor (1).png"))); // NOI18N
+        jButton5.setText("Proveedor");
+        jButton5.setBorder(null);
+
+        jButton6.setBackground(new java.awt.Color(144, 183, 125));
+        jButton6.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ticket (3).png"))); // NOI18N
+        jButton6.setText("Tickets");
+        jButton6.setBorder(null);
+
+        jButton7.setBackground(new java.awt.Color(144, 183, 125));
+        jButton7.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cliente (1).png"))); // NOI18N
+        jButton7.setText("Clientes");
+        jButton7.setBorder(null);
+
+        jButton1.setBackground(new java.awt.Color(144, 183, 125));
+        jButton1.setFont(new java.awt.Font("MS Gothic", 0, 18)); // NOI18N
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/leon (3).png"))); // NOI18N
+        jButton1.setText("Animales");
+        jButton1.setBorder(null);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(btnCerrarSesion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(31, 31, 31))
+            .addComponent(btnCerrarSesion, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 376, Short.MAX_VALUE)
+                .addGap(37, 37, 37)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
                 .addComponent(btnCerrarSesion, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jPanel3.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 240, 630));
+        jPanel3.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 170, 240, 460));
 
         agregarpersona.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/datos-del-usuario.png"))); // NOI18N
         agregarpersona.setText("Personal");
@@ -275,11 +330,6 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
 
         jMIagregarCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/agregar-usuario (2).png"))); // NOI18N
         jMIagregarCliente.setText("Agregar cliente");
-        jMIagregarCliente.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMIagregarClienteActionPerformed(evt);
-            }
-        });
         jMProveedor1.add(jMIagregarCliente);
 
         JMIvistaCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ver.png"))); // NOI18N
@@ -318,14 +368,6 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMIagregarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMIagregarClienteActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMIagregarClienteActionPerformed
-
-    private void btnCerrarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCerrarSesionActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnCerrarSesionActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -338,6 +380,12 @@ public class viewPantallaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuBar barramenus;
     private javax.swing.JButton btnCerrarSesion;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
     private javax.swing.JDesktopPane jDPprincipal;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JMenuItem jMIAgregarDieta;
