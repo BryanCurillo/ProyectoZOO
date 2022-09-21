38a2bdca70b2d3892e7af53b26e2b4c987147d23
Alejo2412qwe <coraspe.alejandro2@gmail.com>
2022-09-20 20:37:01 -0500

    xd

--- ZooM3A/src/model/Dieta.java
+++ ZooM3A/src/model/Dieta.java
@@ -16,6 +16,9 @@ public class Dieta extends Alimento {
     private int die_idAlimento;
     private boolean die_estado;
 
+    public Dieta() {
+    }
+
     public Dieta(int die_id, String die_horario, String die_porcion, int die_idAlimento, boolean die_estado) {
         this.die_id = die_id;
         this.die_horario = die_horario;
@@ -30,6 +33,46 @@ public class Dieta extends Alimento {
         this.die_horario = die_horario;
         this.die_porcion = die_porcion;
         this.die_idAlimento = die_idAlimento;
+        this.die_estado = die_estado;
+    }
+
+    public int getDie_id() {
+        return die_id;
+    }
+
+    public void setDie_id(int die_id) {
+        this.die_id = die_id;
+    }
+
+    public String getDie_horario() {
+        return die_horario;
+    }
+
+    public void setDie_horario(String die_horario) {
+        this.die_horario = die_horario;
+    }
+
+    public String getDie_porcion() {
+        return die_porcion;
+    }
+
+    public void setDie_porcion(String die_porcion) {
+        this.die_porcion = die_porcion;
+    }
+
+    public int getDie_idAlimento() {
+        return die_idAlimento;
+    }
+
+    public void setDie_idAlimento(int die_idAlimento) {
+        this.die_idAlimento = die_idAlimento;
+    }
+
+    public boolean isDie_estado() {
+        return die_estado;
+    }
+
+    public void setDie_estado(boolean die_estado) {
         this.die_estado = die_estado;
     }
 
